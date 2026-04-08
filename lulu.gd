f22166fd7ee73c907e12820a9f218289222ba193
aasujape-cmyk <aasujape@gmail.com>
2026-04-08 09:27:30 +0200

    FIXING AN ERROR THAT OCCURRED WHEN TRYING TO RUN

--- lulu.gd
+++ lulu.gd
@@ -6,7 +6,17 @@ func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	var speed = 200
 	if not action:
-
+		if direction.length() > 0:
+			if Input.is_action_pressed("run"):
+				$AnimatedSprite2D.play("run")
+				speed=400
+				$LuluStamina.value-=10*delta
+			else:
+				$AnimatedSprite2D.play("walk")
+				$LuluStamina.value-=2*delta
+		else:
+			$AnimatedSprite2D.play("idle")
+			$LuluStamina.value+=10*delta
 		if direction.x < 0:
 			$AnimatedSprite2D.flip_h = true  
 		elif direction.x > 0:
@@ -37,20 +47,8 @@ func _physics_process(delta):
 		await $AnimatedSprite2D.animation_finished
 		$LuluStamina.value+=30
 		 
-	if not action:
-		if direction.length() > 0:
-			if Input.is_action_pressed("run"):
-				$AnimatedSprite2D.play("run")
-			
-				$LuluStamina.value-=10*delta
-			else:
-				$AnimatedSprite2D.play("walk")
-				$LuluStamina.value-=2*delta
-		else:
-			$AnimatedSprite2D.play("idle")
-			$LuluStamina.value+=10*delta
-
-
+	#if not action:
+	
 func _on_animated_sprite_2d_animation_finished():
 	if $AnimatedSprite2D.animation=="ladrar":
 		$AttackAura/bark.disabled=true
